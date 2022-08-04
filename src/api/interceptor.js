5b8f96785c798353fbba3432533214be78d3e39d
DevArtifacTs <jessada_sr@hotmail.com>
2022-08-04 10:43:50 +0700

    fix : use https instead http

--- src/api/interceptor.js
+++ src/api/interceptor.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //create axios instance 
 export const axiosInstance = axios.create({
-    baseURL : 'http://api.openweathermap.org/data/2.5',
+    baseURL : 'https://api.openweathermap.org/data/2.5',
 })
 
 
